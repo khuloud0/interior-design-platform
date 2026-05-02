9666ef5f50358eedc002be12cab9b29ac56d8798
RaghadNasiaf <71404779+RaghadNasiaf@users.noreply.github.com>
2026-05-02 16:11:30 +0300

    Implement routing with Login, Signup, and SelectRole

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import SelectRole from "./pages/auth/SelectRole";
 import DesignForm from "./components/DesignForm";
 
 function App() {
   return (
-    <div>
-      <DesignForm />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/select-role" element={<SelectRole />} />
+          <Route path="/design-form" element={<DesignForm />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
-
