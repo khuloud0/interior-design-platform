f7c9f3f254544dd498011bb9f947eeb0e16e21a5
khuloud Hamdan <khuloodalgarni@gmail.com>
2026-05-13 15:06:39 +0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,84 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Signup from "./pages/auth/Signup";
 import Home from "./pages/Home";
-import VerifyEmail from "./pages/auth/VerifyEmail";
-import VerifyPhone from "./pages/auth/VerifyPhone";
+import HomeownerDashboard from "./pages/homeowner/HomeownerDashboard";
+import CreateRequest from "./pages/homeowner/CreateRequest";
+import { RequestDetailsPage } from "./pages/homeowner/RequestDetails";
+import ExploreDesigners from "./pages/designer/ExploreDesigners";
+import DesignerProfile from "./pages/designer/DesignerProfile";
+import DesignerRequestsDashboard from "./pages/designer/DesignerRequestsDashboard";
+import MyProfile from "./pages/designer/MyProfile";
+
+const ClientRoute = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user) return <Navigate to="/" />;
+  if (user.role !== "client") return <Navigate to="/" />;
+  return children;
+};
+
+const DesignerRoute = ({ children }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  if (!user) return <Navigate to="/" />;
+  if (user.role !== "designer") return <Navigate to="/" />;
+  return children;
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/verify-email" element={<VerifyEmail />} />
-        <Route path="/verify-phone" element={<VerifyPhone />} />
+        <Route path="/login"           element={<Login />} />
+        <Route path="/signup"          element={<Signup />} />
+        <Route path="/"                element={<Home />} />
+        <Route path="/designers"       element={<ExploreDesigners />} />
+        <Route path="/designers/:slug" element={<DesignerProfile />} />
+
+        <Route
+          path="/designer/requests"
+          element={
+            <DesignerRoute>
+              <DesignerRequestsDashboard />
+            </DesignerRoute>
+          }
+        />
+
+        <Route
+          path="/designer/MyProfile"
+          element={
+            <DesignerRoute>
+              <MyProfile />
+            </DesignerRoute>
+          }
+        />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ClientRoute>
+              <HomeownerDashboard />
+            </ClientRoute>
+          }
+        />
+
+        <Route
+          path="/create-request"
+          element={
+            <ClientRoute>
+              <CreateRequest />
+            </ClientRoute>
+          }
+        />
+
+        <Route
+          path="/requests/:id"
+          element={
+            <ClientRoute>
+              <RequestDetailsPage />
+            </ClientRoute>
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
