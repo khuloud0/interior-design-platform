1d837928c11d64c4ee3177c0d053ca154ae41dbc
Layan-6 <11956@holbertonstudents.com>
2026-05-19 23:35:45 +0300

    Refactor comments and improve code clarity

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,31 +2,28 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_jwt_extended import JWTManager
 
-# تهيئة المكتبات خارج الدالة
 db = SQLAlchemy()
 jwt = JWTManager()
 
 def create_app():
-    # 1. إنشاء تطبيق Flask
     app = Flask(__name__)
     
-    # 2. تحميل الإعدادات
+    # تحميل الإعدادات
     app.config.from_object("app.config.DevelopmentConfig")
     
-    # 3. تهيئة الإضافات وتمرير التطبيق لها
+    # تهيئة الإضافات
     db.init_app(app)
     jwt.init_app(app)
     
-    # 4. استيراد الموديلات (مهم جداً قبل create_all ليتعرف عليها الـ ORM)
+    # يتعرف عليها قبل إنشاء الجداول استيراد المودلز عشان SQLAlcheme
     from app.models import User, ProviderProfile, DesignerProfile
     
-    # 5. إنشاء الجداول في قاعدة البيانات (PostgreSQL)
+    # من المودلز إنشاء الجداول في PostgreSQL #
     with app.app_context():
         db.create_all()
         
-    # 6. تسجيل الـ Routes والـ Blueprints
+    # تسجيل الـ routes
     from app.routes.auth_routes import auth_bp
     app.register_blueprint(auth_bp)
     
-    # 7. إرجاع كائن التطبيق
     return app
